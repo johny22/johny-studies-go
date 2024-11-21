b9727c9fb7814af9797817a6d2f9d68bdab7c0cd
Jones R. Bezerra <jones.rom22@gmail.com>
2024-11-21 15:45:14 -0300

    Improving my greetings

--- hello/hello.go
+++ hello/hello.go
@@ -4,6 +4,8 @@ import "fmt"
 import "rsc.io/quote"
 
 func main() {
-	fmt.Println("Hello, Jones! Let's GO!")
-	fmt.Println(quote.Go())
+	fmt.Println("Hello, Jones! Let's GO!\n\n")
+	fmt.Println(quote.Go() + "\n\t\u03a9", '!') // It uses ' for sigle character and " for strings :)
+	fmt.Println(quote.Opt() + "\n\n")
+	fmt.Println(quote.Glass() + "\t:v\n\n")
 }
