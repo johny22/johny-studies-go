983ad1e7da6aeef8b50635f1502b1f1b07617ed7
Jones R. Bezerra <jones.rom22@gmail.com>
2025-02-04 19:06:03 -0300

    Learning about custom types on golang

--- basic-types/basic-types.go
+++ basic-types/basic-types.go
@@ -6,12 +6,16 @@ import (
 	"math/cmplx"
 )
 
+type fiambre int
+
 var (
-	ToBe   bool       = 1 == 1
-	MaxInt uint64     = 1<<64 - 1
-	Aurum  float64    = math.Phi
-	z      complex128 = cmplx.Sqrt(-5 + 12i)
-	x, y   rune       = '🐺', '🐍' // Unicode code point rune is an alias for int32
+	ToBe      bool       = 1 == 1
+	MaxInt    uint64     = 1<<64 - 1
+	Aurum     float64    = math.Phi
+	z         complex128 = cmplx.Sqrt(-5 + 12i)
+	x, y      rune       = '🐺', '🐍' // Unicode code point rune is an alias for int32
+	falseMeat fiambre    = 10
+	bread     int        = 11
 )
 
 func main() {
@@ -19,5 +23,7 @@ func main() {
 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
 	fmt.Printf("Type: %T Value: %v\n", Aurum, Aurum)
 	fmt.Printf("Type: %T Value: %v\n", z, z)
+	fmt.Printf("Type: %T Value: %v\n", falseMeat, falseMeat)
+	fmt.Printf("My hotdog costs R$ %v.\n", int(falseMeat)+bread) // OMG that's not even healthy
 	fmt.Printf("Type: %T Test =:> %c & %c => %d\n", y, x, y, x+y)
 }
--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,29 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("Meet my friend... יֵשׁוּעַ") // Yeshua!
-	fmt.Println(quote.Go() + "\n\t\u03a9", '!') // It uses ' for sigle character and " for strings :)
+	fmt.Println(quote.Go()+"\n\t\u03a9", '!') // It uses ' for sigle character and " for strings :)
+
+	var names = [4]string{"Juana", "Judite", "Jacinta", "Josefa"}
+	myslice1 := []int{} //What?
+	fmt.Println(len(myslice1))
+	fmt.Println(cap(myslice1))
+	fmt.Println(myslice1)
+
+	myslice2 := []string{"Go", "stranges", "are", "fast,", "but goodies. Squouiiiirrel!"}
+	fmt.Println(len(myslice2))
+	fmt.Println(cap(myslice2))
+	fmt.Println(myslice2)
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
 }
 
 // Then... Maranata!
